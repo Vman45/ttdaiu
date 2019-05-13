2ab01e6a777ce1d3103185526cd448b39c64d24d
Carlos Rabelo <developer@carlosrabelo.com.br>
2019-05-13 19:05:14 -0400

    2019-005-13 19:05

--- docker-compose.sh
+++ docker-compose.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-base=https://github.com/docker/compose/releases/download/1.23.1
+base=https://github.com/docker/compose/releases/download/1.24.0
 
 curl -L $base/docker-compose-$(uname -s)-$(uname -m) >/tmp/docker-compose
 
