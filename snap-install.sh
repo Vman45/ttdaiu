057d7ef798c1833fc11a0b338e50ec8f24845b88
Carlos Rabelo <developer@carlosrabelo.com.br>
2019-06-11 07:50:16 -0400

    2019-06-11 07:50

---
+++ snap-install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+snap install android-studio --classic
+snap install eclipse --classic
+snap install godot --classic
+snap install hugo
+snap install intellij-idea-community --classic
+snap install kubectl --classic
+snap install powershell --classic
+snap install pycharm-community --classic
+snap install slack --classic
